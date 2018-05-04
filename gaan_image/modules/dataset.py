31bc9109aacf33015aa584f95e2cd04695d1608b
Trung Tran <tntrung@gmail.com>
2018-05-04 23:58:53 +0800

    Add GAAn for CelebA

--- gaan_image/modules/dataset.py
+++ gaan_image/modules/dataset.py
@@ -1,6 +1,12 @@
 import math
+import glob
+import os
 import numpy as np
 import tensorflow as tf
+
+# for image io
+from skimage import io
+from skimage.transform import resize
 
 # for mnist
 from tensorflow.examples.tutorials.mnist import input_data
@@ -8,12 +14,24 @@ from tensorflow.examples.tutorials.mnist import input_data
 # for cifar10
 import cPickle as pickle
 
+# List all dir with specific name 
+def list_dir(folder_dir, ext="png"):
+    all_dir = sorted(glob.glob(folder_dir+"*."+ext), key=os.path.getmtime)
+    return all_dir
+
 def unpickle(file):
     fo = open(file, 'rb')
     dict = pickle.load(fo)
     fo.close()
     return dict['data'], dict['labels']
-    
+
+def imread(path, is_grayscale=False):
+    if (is_grayscale):
+        img = io.imread(path, is_grayscale=True).astype(np.float)
+    else:
+        img = io.imread(path).astype(np.float)
+    return np.array(img)  
+        
 def read_cifar10(data_dir, filenames):
     all_data = []
     all_labels = []
@@ -34,97 +52,157 @@ def read_cifar10(data_dir, filenames):
     print('cifar10 data: {}'.format(np.shape(images)))
     return images, labels
 
+# processing for celeba dataset
+def preprocess(img):
+    crop_size = 108
+    re_size   = 64
+    top_left  = [(218 - crop_size)//2, (178 - crop_size)//2]
+    img       = img[top_left[0]:top_left[0]+crop_size, top_left[1]:top_left[1]+crop_size, :]
+    img       = resize(img, [re_size, re_size])
+    return img
+    
 class Dataset(object):
 
-	def __init__(self, name='mnist', source='./data/mnist/', one_hot=True, batch_size = 64, seed = 0):
+    def __init__(self, name='mnist', source='./data/mnist/', one_hot=True, batch_size = 64, seed = 0):
 
 
-		self.name    		 = name
-		self.source  		 = source
-		self.one_hot 		 = one_hot
-		self.batch_size      = batch_size
-		self.seed            = seed
+        self.name            = name
+        self.source          = source
+        self.one_hot         = one_hot
+        self.batch_size      = batch_size
+        self.seed            = seed
+        np.random.seed(seed) # To make your "random" minibatches the same as ours
 
-		self.count           = 0
+        self.count           = 0
 
-		tf.set_random_seed(self.seed)  # Fix the random seed for randomized tensorflow operations.
+        tf.set_random_seed(self.seed)  # Fix the random seed for randomized tensorflow operations.
 
-		if name == 'mnist':
-			self.mnist = input_data.read_data_sets(source)
-			self.data  = self.mnist.train.images
-			print('data shape: {}'.format(np.shape(self.data)))
-		elif name == 'cifar10':
-			# download data files from: 'http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz' 
-			# extract into the correct folder
-			data_files = ['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5']
-			self.data, _ = read_cifar10(source, data_files)
-			
-		self.minibatches = self.random_mini_batches(self.data.T, self.batch_size, self.seed)
+        if name == 'mnist':
+            self.mnist = input_data.read_data_sets(source)
+            self.data  = self.mnist.train.images
+            print('data shape: {}'.format(np.shape(self.data)))
+            self.minibatches = self.random_mini_batches(self.data.T, self.batch_size, self.seed)
+        elif name == 'cifar10':
+            # download data files from: 'http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz' 
+            # extract into the correct folder
+            data_files = ['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5']
+            self.data, _ = read_cifar10(source, data_files)
+            self.minibatches = self.random_mini_batches(self.data.T, self.batch_size, self.seed)
+        elif name == 'celeba':
+            # Count number of data images
+            self.im_list  = list_dir(source, 'jpg')
+            self.nb_imgs  = len(self.im_list)
+            self.nb_compl_batches  = int(math.floor(self.nb_imgs/self.batch_size))
+            self.nb_total_batches     = self.nb_compl_batches
+            if self.nb_imgs % batch_size != 0:
+               self.num_total_batches = self.nb_compl_batches + 1
+            self.count = 0
+            self.color_space = 'RGB'
 
-	def db_name(self):
-		return self.name
+    def db_name(self):
+        return self.name
 
-	def data_dim(self):
-		if self.name == 'mnist':
-			return 784  #28x28
-		elif self.name == 'cifar10':
-			return 3072 #32x32x3
-		else:
-			print('data_dim is unknown.\n')
+    def data_dim(self):
+        if self.name == 'mnist':
+            return 784  #28x28
+        elif self.name == 'cifar10':
+            return 3072 #32x32x3
+        elif self.name == 'celeba':
+            return 12288 #64x64x3
+        else:
+            print('data_dim is unknown.\n')
 
-	def data_shape(self):
-		if self.name == 'mnist':
-			return [28, 28, 1]
-		elif self.name == 'cifar10':
-			return [32, 32, 3]
-		else:
-			print('data_dim is unknown.\n')
-			
-	def mb_size(self):
-		return self.batch_size
+    def data_shape(self):
+        if self.name == 'mnist':
+            return [28, 28, 1]
+        elif self.name == 'cifar10':
+            return [32, 32, 3]
+        elif self.name == 'celeba':
+            return [64, 64, 3]
+        else:
+            print('data_shape is unknown.\n')
+            
+    def mb_size(self):
+        return self.batch_size
 
-	def next_batch(self):
+    def next_batch(self):
 
-		if self.count == len(self.minibatches):
-			self.count = 0
-			self.minibatches = self.random_mini_batches(self.data.T, self.batch_size, self.seed)
+        if self.name == 'mnist' or self.name == 'cifar10':
+            if self.count == len(self.minibatches):
+                self.count = 0
+                self.minibatches = self.random_mini_batches(self.data.T, self.batch_size, self.seed)
+            batch = self.minibatches[self.count]
+            self.count = self.count + 1
+            return batch.T
+        elif self.name == 'celeba':
+            batch = self.random_mini_batches([], self.batch_size, self.seed)
+            return batch
 
-		batch = self.minibatches[self.count]
-		self.count = self.count + 1
-		#if self.count % 10 == 0:
-		#    print('Minibatch count = {} / {}'.format(self.count, len(self.minibatches)))
-		return batch.T
+    # Random minibatches for training
+    def random_mini_batches(self, X, mini_batch_size = 64, seed = 0):
+        """
+        Creates a list of random minibatches from (X)
+        
+        Arguments:
+        X -- input data, of shape (input size, number of examples)
+        mini_batch_size -- size of the mini-batches, integer
+        
+        Returns:
+        mini_batches -- list of synchronous (mini_batch_X)
+        """
+        
+        if self.name == 'mnist' or self.name == 'cifar10':
+            m = X.shape[1]                  # number of training examples
+            mini_batches = []
+                
+            # Step 1: Shuffle (X, Y)
+            permutation = list(np.random.permutation(m))
+            shuffled_X = X[:, permutation]
 
-	# Random minibatches for training
-	def random_mini_batches(self, X, mini_batch_size = 64, seed = 0):
-	    """
-	    Creates a list of random minibatches from (X)
-	    
-	    Arguments:
-	    X -- input data, of shape (input size, number of examples)
-	    mini_batch_size -- size of the mini-batches, integer
-	    
-	    Returns:
-	    mini_batches -- list of synchronous (mini_batch_X)
-	    """
-	    
-	    np.random.seed(seed)            # To make your "random" minibatches the same as ours
-	    m = X.shape[1]                  # number of training examples
-	    mini_batches = []
-	        
-	    # Step 1: Shuffle (X, Y)
-	    permutation = list(np.random.permutation(m))
-	    shuffled_X = X[:, permutation]
+            # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+            num_complete_minibatches = int(math.floor(m/self.batch_size)) # number of mini batches of size mini_batch_size in your partitionning
+            for k in range(0, num_complete_minibatches):
+                mini_batch_X = shuffled_X[:, k * self.batch_size : (k+1) * self.batch_size]
+                mini_batches.append(mini_batch_X)
+            
+            # Handling the end case (last mini-batch < mini_batch_size)
+            if m % mini_batch_size != 0:
+                mini_batch_X = shuffled_X[:, num_complete_minibatches * self.batch_size : m]
+                mini_batches.append(mini_batch_X)
+            
+            return mini_batches
+            
+        elif self.name == 'celeba':
+            
+            if self.count == 0:
+                self.permutation = list(np.random.permutation(self.nb_imgs))
+                cur_batch = self.permutation[self.count * self.batch_size : (self.count + 1) * self.batch_size]  
+            elif self.count > 0 and self.count < self.nb_compl_batches:
+                cur_batch = self.permutation[self.count * self.batch_size : (self.count + 1) * self.batch_size]                           
+            elif self.count == self.nb_compl_batches and self.num_total_batches > self.nb_compl_batches:
+                cur_batch = self.permutation[self.nb_compl_batches * self.batch_size : self.nb_imgs]
+            elif self.count >= self.num_total_batches:
+                self.count = 0
+                self.permutation = list(np.random.permutation(self.nb_imgs))
+                cur_batch = self.permutation[self.count * self.batch_size : (self.count + 1) * self.batch_size]                
+            else:
+                print('something is wrong with mini-batches')
+            
+            mini_batches = []
 
-	    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
-	    num_complete_minibatches = int(math.floor(m/self.batch_size)) # number of mini batches of size mini_batch_size in your partitionning
-	    for k in range(0, num_complete_minibatches):
-	        mini_batch_X = shuffled_X[:, k * self.batch_size : (k+1) * self.batch_size]
-	        mini_batches.append(mini_batch_X)
-	    
-	    # Handling the end case (last mini-batch < mini_batch_size)
-	    if m % mini_batch_size != 0:
-	        mini_batch_X = shuffled_X[:, num_complete_minibatches * self.batch_size : m]
-	        mini_batches.append(mini_batch_X)
-	    
-	    return mini_batches
+            # handle complete cases
+            for k in cur_batch:
+                img = imread(self.im_list[k])
+                #print('loading: {}'.format(self.im_list[k]))
+                if self.name == 'celeba':
+                    img = preprocess(img)
+                if self.color_space == 'YUV':
+                    img = RGB2YUV(img)
+                img = img / 255.0
+                #print('img shape: {}'.format(np.shape(img)))
+                mini_batches.append(np.reshape(img,(1,np.shape(img)[0] * np.shape(img)[1] * np.shape(img)[2])))
+            
+            mini_batches = np.concatenate(mini_batches, axis=0)
+            self.count = self.count + 1
+                    
+            return mini_batches
--- gaan_image/modules/models.py
+++ gaan_image/modules/models.py
@@ -18,53 +18,7 @@ max_pooling = partial(slim.max_pool2d, kernel_size=2, stride=2, padding='VALID')
 batch_norm = partial(slim.batch_norm, decay=0.9, scale=True, epsilon=1e-5, updates_collections=None)
 
 '''
-Common encoder/generator/discriminator
-'''
-def encoder(img, x_shape, z_dim=128, dim=64, kernel_size=5, stride=2, name = 'encoder', reuse=True, training=True):
-    bn = partial(batch_norm, is_training=training)
-    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
-
-    y = tf.reshape(img,[-1, x_shape[0], x_shape[1], x_shape[2]])
-    with tf.variable_scope(name, reuse=reuse):
-        y = lrelu(conv(y, dim, kernel_size, stride))
-        y = conv_bn_lrelu(y, dim * 2, kernel_size, stride)
-        y = conv_bn_lrelu(y, dim * 4, kernel_size, stride)
-        y = conv_bn_lrelu(y, dim * 8, kernel_size, stride)
-        logit = fc(y, z_dim)
-        return logit
-
-def generator(z, x_shape, dim=64, kernel_size=5, stride=2, name = 'generator', reuse=True, training=True):
-    bn = partial(batch_norm, is_training=training)
-    dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
-    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
-
-    x_dim = x_shape[0] * x_shape[1] * x_shape[2]
-    with tf.variable_scope(name, reuse=reuse):
-        y = fc_bn_relu(z, 2 * 2 * dim * 8)
-        y = tf.reshape(y, [-1, 2, 2, dim * 8])
-        y = dconv_bn_relu(y, dim * 4, kernel_size, stride)
-        y = dconv_bn_relu(y, dim * 2, kernel_size, stride)
-        y = dconv_bn_relu(y, dim * 1, kernel_size, stride)
-        y = dconv(y, x_shape[2], kernel_size, stride)
-        y = tf.reshape(y, [-1, x_dim])
-        return tf.sigmoid(y)
-               
-def discriminator(img, x_shape, dim=64, kernel_size=5, stride=2, name='discriminator', reuse=True, training=True):
-    bn = partial(batch_norm, is_training=training)
-    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
-
-    y = tf.reshape(img,[-1, x_shape[0], x_shape[1], x_shape[2]])
-    with tf.variable_scope(name, reuse=reuse):
-        y = lrelu(conv(y, dim, kernel_size, 2))
-        y = conv_bn_lrelu(y, dim * 2, kernel_size, stride)
-        y = conv_bn_lrelu(y, dim * 4, kernel_size, stride)
-        feature = y
-        y = conv_bn_lrelu(y, dim * 8, kernel_size, stride)
-        logit = fc(y, 1)
-        return tf.nn.sigmoid(logit), logit, tf.reshape(feature,[tf.shape(img)[0], -1])
-
-'''
-The encoder/generator/discriminator for MNIST (smaller network for 32x32 images)
+The encoder/generator/discriminator for MNIST (smaller network for 28x28 images)
 '''
 
 def encoder_mnist(img, x_shape, z_dim=128, dim=64, kernel_size=5, stride=2, name = 'encoder', reuse=True, training=True):
@@ -126,4 +80,98 @@ def discriminator_mnist(img, x_shape, dim=64, kernel_size=5, stride=2, name='dis
         feature = y
         logit = fc(y, 1)
         print(logit.get_shape())
+        return tf.nn.sigmoid(logit), logit, tf.reshape(feature,[tf.shape(img)[0], -1])
+
+
+'''
+Common encoder/generator/discriminator for cifar-10 (32x32)
+'''
+def encoder_cifar(img, x_shape, z_dim=128, dim=64, kernel_size=5, stride=2, name = 'encoder', reuse=True, training=True):
+    bn = partial(batch_norm, is_training=training)
+    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+
+    y = tf.reshape(img,[-1, x_shape[0], x_shape[1], x_shape[2]])
+    with tf.variable_scope(name, reuse=reuse):
+        y = lrelu(conv(y, dim, kernel_size, stride))
+        y = conv_bn_lrelu(y, dim * 2, kernel_size, stride)
+        y = conv_bn_lrelu(y, dim * 4, kernel_size, stride)
+        y = conv_bn_lrelu(y, dim * 8, kernel_size, stride)
+        logit = fc(y, z_dim)
+        return logit
+
+def generator_cifar(z, x_shape, dim=64, kernel_size=5, stride=2, name = 'generator', reuse=True, training=True):
+    bn = partial(batch_norm, is_training=training)
+    dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+
+    x_dim = x_shape[0] * x_shape[1] * x_shape[2]
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_bn_relu(z, 2 * 2 * dim * 8)
+        y = tf.reshape(y, [-1, 2, 2, dim * 8])
+        y = dconv_bn_relu(y, dim * 4, kernel_size, stride)
+        y = dconv_bn_relu(y, dim * 2, kernel_size, stride)
+        y = dconv_bn_relu(y, dim * 1, kernel_size, stride)
+        y = dconv(y, x_shape[2], kernel_size, stride)
+        y = tf.reshape(y, [-1, x_dim])
+        return tf.sigmoid(y)
+               
+def discriminator_cifar(img, x_shape, dim=64, kernel_size=5, stride=2, name='discriminator', reuse=True, training=True):
+    bn = partial(batch_norm, is_training=training)
+    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+
+    y = tf.reshape(img,[-1, x_shape[0], x_shape[1], x_shape[2]])
+    with tf.variable_scope(name, reuse=reuse):
+        y = lrelu(conv(y, dim, kernel_size, 2))
+        y = conv_bn_lrelu(y, dim * 2, kernel_size, stride)
+        y = conv_bn_lrelu(y, dim * 4, kernel_size, stride)
+        y = conv_bn_lrelu(y, dim * 8, kernel_size, stride)
+        feature = y
+        logit = fc(y, 1)
+        return tf.nn.sigmoid(logit), logit, tf.reshape(feature,[tf.shape(img)[0], -1])
+
+
+'''
+Common encoder/generator/discriminator for celeba (64x64)
+'''
+def encoder_celeba(img, x_shape, z_dim=128, dim=64, kernel_size=5, stride=2, name = 'encoder', reuse=True, training=True):
+    bn = partial(batch_norm, is_training=training)
+    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+
+    y = tf.reshape(img,[-1, x_shape[0], x_shape[1], x_shape[2]])
+    with tf.variable_scope(name, reuse=reuse):
+        y = lrelu(conv(y, dim, kernel_size, stride))        #[32 x 32 x dim]
+        y = conv_bn_lrelu(y, dim * 2, kernel_size, stride)  #[16 x 16 x 2 x dim]
+        y = conv_bn_lrelu(y, dim * 4, kernel_size, stride)  #[8 x 8 x 4 x dim]
+        y = conv_bn_lrelu(y, dim * 8, kernel_size, stride)  #[4 x 4 x 8 x dim]
+        logit = fc(y, z_dim)                                #[z_dim]
+        return logit
+
+def generator_celeba(z, x_shape, dim=64, kernel_size=5, stride=2, name = 'generator', reuse=True, training=True):
+    bn = partial(batch_norm, is_training=training)
+    dconv_bn_relu = partial(dconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    fc_bn_relu = partial(fc, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+
+    x_dim = x_shape[0] * x_shape[1] * x_shape[2]
+    with tf.variable_scope(name, reuse=reuse):
+        y = fc_bn_relu(z, 4 * 4 * dim * 8)
+        y = tf.reshape(y, [-1, 2, 2, dim * 8])
+        y = dconv_bn_relu(y, dim * 4, kernel_size, stride)
+        y = dconv_bn_relu(y, dim * 2, kernel_size, stride)
+        y = dconv_bn_relu(y, dim * 1, kernel_size, stride)
+        y = dconv(y, x_shape[2], kernel_size, stride)
+        y = tf.reshape(y, [-1, x_dim])
+        return tf.sigmoid(y)
+               
+def discriminator_celeba(img, x_shape, dim=64, kernel_size=5, stride=2, name='discriminator', reuse=True, training=True):
+    bn = partial(batch_norm, is_training=training)
+    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+
+    y = tf.reshape(img,[-1, x_shape[0], x_shape[1], x_shape[2]])
+    with tf.variable_scope(name, reuse=reuse):
+        y = lrelu(conv(y, dim, kernel_size, 2))
+        y = conv_bn_lrelu(y, dim * 2, kernel_size, stride)
+        y = conv_bn_lrelu(y, dim * 4, kernel_size, stride)
+        y = conv_bn_lrelu(y, dim * 8, kernel_size, stride)
+        feature = y
+        logit = fc(y, 1)
         return tf.nn.sigmoid(logit), logit, tf.reshape(feature,[tf.shape(img)[0], -1])
